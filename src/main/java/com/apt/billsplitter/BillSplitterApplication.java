f7e8c17eb1ecb6a509b67881b10d1ad156a1291a
bista2054 <sbista33208@ucumberlands.edu>
2025-03-09 00:43:13 -0600

    initial commit

---
+++ src/main/java/com/apt/billsplitter/BillSplitterApplication.java
@@ -0,0 +1,13 @@
+package com.apt.billsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSplitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apt/billsplitter/BillSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apt.billsplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
